1fce7e4a154ae93fff3bac28b4a6b0acd58e5a37
Sabeena <sabeenarayyan13@gmail.com>
2021-12-01 14:38:56 -0600

    Added homepage and search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
